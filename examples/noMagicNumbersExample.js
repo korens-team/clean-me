9caa0577462f607c2ba79d67114ae9fa3bd0873d
erezasu <erezasu@post.bgu.ac.il>
2019-02-13 08:35:37 -0800

    update'

--- examples/noMagicNumbersExample.js
+++ examples/noMagicNumbersExample.js
@@ -1,14 +1,28 @@
 function a(b,c){
-  return 8
+  if(a==8) {
+
+    return 8
+  }
 }
+
+
+
+ r = () => {
+  return 7
+}
+
 var d = 5
 
 while(a!= 7){
+  const c = 8
   switch(a){
     case 4:
       break
   }
 }
 
-if(a != 6){a(d,3)}
+if(a != 6){
+  const y = 4
+  a(d,3)
+}
 
