fcbd91a135059d567607014a2504a3c93e062378
erezasu <erezasu@post.bgu.ac.il>
2019-02-13 04:51:08 -0800

    'noMagicNumbers'

--- rules/magicNumbers.js
+++ rules/magicNumbers.js
@@ -7,7 +7,8 @@ class magicNumbers {
                 /*console.log("node: " + node.type)
                 if(parent)console.log("parent: " + parent.type)*/
                 if(parent && parent.type != "VariableDeclarator" && node.type == "Literal"){
-                    console.log(node)
+                   //console.log(node)
+                    console.log("You have a magic number on row " + node.loc.start.line + " at column " + node.loc.start.column)
                 }
             }
         })    
