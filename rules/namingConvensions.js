7e8935f89d8359a89b39bf29f113606b82f29ba9
ofirelarat <96ofir11@gmail.com>
2019-02-13 14:01:35 +0200

    convension rule logic

--- rules/namingConvensions.js
+++ rules/namingConvensions.js
@@ -1,14 +1,31 @@
 const estraverse = require("estraverse")
 
+const VariableDeclaration_type = "VariableDeclaration";
+const VariableDeclarator_type = "VariableDeclarator";
+
 class NamingConvensions {
     static apply(syntaxTree) {
+
+        let declerationsArray = [];
+        let index = 0;
+
         estraverse.traverse(syntaxTree, {
             enter: (node) => {
-                console.log(node)
+                if(node.type == VariableDeclaration_type){
+                    declerationsArray.push({
+                        "kind": node.kind,
+                        "row": node.loc.start.line   
+                    });
+                }
+                /*if(node.type == VariableDeclarator_type){
+                    declerationsArray[index].name = node.id.name;
+                    index++;
+                }*/
             }
         });
+
+        console.log(declerationsArray);
     };
 }
 
 module.exports = NamingConvensions;
-
