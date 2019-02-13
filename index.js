b12fcbe58b7d9fcfe66e11b7f432d42c15783b1b
ofirelarat <96ofir11@gmail.com>
2019-02-13 14:20:45 +0200

    add loc option and remove launch json

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const run = () => {
             const filePath = process.argv[index + 1];
             if(filePath){
               if (fs.existsSync(filePath)) {                     
-                ast = esprima.parse(fs.readFileSync(filePath, 'utf-8'))
+                ast = esprima.parse(fs.readFileSync(filePath, 'utf-8'), {loc: true})
               } else{
                 console.error("missing file input");
               }
