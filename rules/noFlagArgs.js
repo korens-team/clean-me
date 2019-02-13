95706dd6d611ad4167f9c2764bfd92d78c1dc875
Koren <korenzerah123@gmail.com>
2019-02-13 14:10:47 +0200

    break

--- rules/noFlagArgs.js
+++ rules/noFlagArgs.js
@@ -2,15 +2,25 @@ const codegen = require("escodegen")
 const estraverse = require("estraverse")
 
 class noFlagArgs {
+    static functions = []
     static apply(syntaxTree) {
+        getAllFunctions(syntaxTree)
         estraverse.traverse(syntaxTree, {
             enter: (node, parent) => {
                 console.log(node)
             }
-        })    
+        })
     }
 
-
+    static getAllFunctions(syntaxTree) {
+        estraverse.traverse(syntaxTree, {
+            enter: (node, parent) => {
+                if (node.type == 'FunctionDeclaration') {
+                    functions.push(node)
+                }
+            }
+        })
+    }
 }
 
 module.exports = noFlagArgs
