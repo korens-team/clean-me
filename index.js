3a87f76dfd80e3f19609c29d589b49ee230d39c7
Koren <korenzerah123@gmail.com>
2019-02-13 11:48:49 +0200

    add rules enums and switch case

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const shell = require("shelljs");
 const path = require('path'); 
 const fs =  require('fs'); 
 
+const rulesEnum = require('./rulesEnum')
+
 const init = () => {
     console.log(
       chalk.green(
@@ -28,17 +30,17 @@ const run = () => {
             const filePath = process.argv[index + 1];
             if(filePath){
                 if (fs.existsSync(filePath)) { 
-                    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-                        if (!err) {
-                            console.log('file data: ' + data);
-                        } else {
-                            console.log(err);
-                        }
-                    });
+                    
                 } else{
                     console.error("missing file input");
                 }
             }
+        } else {
+          switch(val) {
+            case(rulesEnum.namingConventions): {
+              console.log('haa')
+            }
+          }
         }
       });
   };
---
+++ rulesEnum.js
@@ -0,0 +1,3 @@
+module.exports = {
+    namingConventions: "-namingConventions"
+}
