f948fbe3fa9222e15ac5f402a1c0f562936fb2a6
ofirelarat <96ofir11@gmail.com>
2019-03-12 19:58:34 +0200

    commit

---
+++ src/rules/consts/rulesEnum.js
@@ -0,0 +1,5 @@
+export const NAMING_CONVENTIONS = "namingConventions";
+export const NO_FLAG_ARGS = "noFlagArgs";
+export const NO_SIDE_EFFECTS = "noSideEffects";
+export const NO_MAGIC_NUMBERS = "noMagicNumbers";
+export const ENCAPSULATE_CONDITIONS = "encapsulateConditions";
